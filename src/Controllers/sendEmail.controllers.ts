70513853c419121147c6ea4fb8aecbcf78a98d02
manasdeo <rmnslogin@gmail.com>
2024-06-03 12:34:07 +0530

    nodemailer

--- src/Controllers/sendEmail.controllers.ts
+++ src/Controllers/sendEmail.controllers.ts
@@ -1,7 +1,31 @@
 import { type Request, type Response } from "express";
+import nodemailer from "nodemailer";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const sendEmail = async (req: Request, res: Response) => {
-  res.send("Send Email function");
+  //   let testAccount = await nodemailer.createTestAccount();
+  console.log("Email", process.env.EMAILID);
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "presley.baumbach28@ethereal.email",
+      pass: "Cgg61dUbscM3k8gz2U",
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: '"Manas Deo 👻" <rmnslogin@gmail.com>', // sender address
+    to: "rmnsdeo@gmail.com, baz@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  res.json(info);
 };
 
 export { sendEmail };
